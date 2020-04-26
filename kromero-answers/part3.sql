ab7fb021fbeb94a3a79f6d22667d82259f204268
k-romero <imkevinromero@gmail.com>
2020-04-26 12:56:54 -0400

    modified

--- kromero-answers/part3.sql
+++ kromero-answers/part3.sql
@@ -1,6 +1,12 @@
 SELECT * FROM pokemons JOIN types ON pokemons.primary_type = types.id;
 
 -- w/o join
-SELECT pokemons.name, types.name
+SELECT pokemons.name AS pName, types.name AS tName
 FROM pokemons, types
 WHERE pokemons.primary_type = types.id;
+
+-- with aliases & order
+SELECT pokemons.name AS pName, types.name AS tName
+FROM pokemons, types
+WHERE pokemons.primary_type = types.id
+ORDER BY pName ASC;
