4b74c5ec09117153c272e3cbf67b9d2871bb3d49
k-romero <imkevinromero@gmail.com>
2020-04-26 12:28:17 -0400

    part 2 complete

--- kromero-answers/part2.sql
+++ kromero-answers/part2.sql
@@ -2,3 +2,8 @@ SELECT * FROM types;
 
 SELECT name FROM pokemons WHERE id = 45;
 
+SELECT COUNT(*) FROM pokemons;
+
+SELECT COUNT(*) FROM types;
+
+SELECT COUNT(*) FROM pokemons WHERE secondary_type NOTNULL;
